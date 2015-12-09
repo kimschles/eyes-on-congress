1c9cf26cbb890dd2cf8a8e08249110069f71aacd
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-12-09 10:14:29 -0700

    working to bind state name with searchTerm object

--- js/searchcontroller.js
+++ js/searchcontroller.js
@@ -17,5 +17,43 @@ app.controller('SearchController', ["$scope", "$location", 'DataFactory', 'Searc
       SearchFactory.post({candName: CurrentCandidate.firstlast, party: CurrentCandidate.party, office: CurrentCandidate.office})
       window.location.href = '/#/show/' + CurrentCandidate.cid + '/' + CurrentCandidate.lastname;
     }
-  }
+  } // original penultimate curly brace
+  $scope.mapObject = {
+  scope: 'usa',
+  options: {
+    width: 1110,
+    legendHeight: 60 // optionally set the padding for the legend
+  },
+  geographyConfig: {
+    highlighBorderColor: '#EAA9A8',
+    highlighBorderWidth: 2
+  },
+  fills: {
+    'HIGH': '#CC4731',
+    'MEDIUM': '#306596',
+    'LOW': '#667FAF',
+    'defaultFill': '#DDDDDD'
+  },
+  data: {
+    "AZ": {
+      "fillKey": "MEDIUM",
+    },
+    "CO": {
+      "fillKey": "HIGH",
+    },
+    "DE": {
+      "fillKey": "LOW",
+    },
+    "GA": {
+      "fillKey": "MEDIUM",
+    }
+  },
+}
+$scope.updateActiveGeography = function(geography) {
+  $scope.stateName = geography.properties.name;
+  $scope.stateCode = geography.id;
+}
+
 }]);
+
+// Goal: click on a state, and display that name on the page
